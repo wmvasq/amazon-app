a90d75bcb92e131c39c561ce5c14b48843fbd2db
Marvin Vasquez <wmvasq1@gmail.com>
2023-07-27 01:36:16 -0400

    funcionalidad completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppService, } from './services/app.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ export class AppComponent {
   showModal = false;
   selectedApp: any;
   categories: any = [
-   // { id: '0', name: 'Todas las categorías' }, 
+    // { id: '0', name: 'Todas las categorías' }, 
     //{ id: '1', name: 'Categoria 1' },
-   // { id: '2', name: 'Categoria 2' },
+    // { id: '2', name: 'Categoria 2' },
   ];
-
+  selectedRating: number = 0;
+  showSuccessMessage = false;
   constructor(private appService: AppService) { }
   ngOnInit(): void {
     this.getCategorias();
@@ -30,11 +32,11 @@ export class AppComponent {
       }
     );
   }
-  selectedCategory: number = 0; 
+  selectedCategory: number = 0;
   onCategorySelect(categoryId: number): void {
-    
+
     this.selectedCategory = categoryId;
-    
+
   }
   openModal(app: any) {
     this.selectedApp = app;
@@ -43,5 +45,30 @@ export class AppComponent {
 
   closeModal() {
     this.showModal = false;
+  }
+
+  //
+  //actualizar calificacion
+  actualizarCalificacion(rating: number): void {
+    this.selectedApp.calificacion = rating;
+
+    this.appService.actualizarCalificacion(this.selectedApp.id, this.selectedApp.calificacion).subscribe(
+      (gg) => {
+
+        //controllar error
+        if (gg.status == 200)
+          this.mostrarMensajeExito();
+      },
+      (error) => {
+        console.error('Error al procesar resp:', error);
+      }
+    );
+    this.mostrarMensajeExito();
+  }
+  mostrarMensajeExito(): void {
+    this.showSuccessMessage = true;
+    setTimeout(() => {
+      this.showSuccessMessage = false;
+    }, 3000); // Desaparecerá después de 3 segundos (3000 ms)
   }
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,26 +1,35 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Category } from "../models/Category";
 import { App } from "../models/App";
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class AppService {
-    private apiUrl = 'http://localhost:8080/api/'; 
-  
-    constructor(private http: HttpClient) { }
-  
-    getCategorias(): Observable<Category[]> {
-      return this.http.get<Category[]>(this.apiUrl+"categorias");
-    }
-    getApps(categoria:any): Observable<App[]> {
-     // return this.http.get<App[]>(this.apiUrl+"apps");
-     let url = this.apiUrl + 'apps';
+  providedIn: 'root'
+})
+export class AppService {
+  private apiUrl = 'http://localhost:8080/api/';
+
+  constructor(private http: HttpClient) { }
+  //obtener categorias 
+  getCategorias(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl + "categorias");
+  }
+  //filtrar apps
+  getApps(categoria: any): Observable<App[]> {
+    // return this.http.get<App[]>(this.apiUrl+"apps");
+    let url = this.apiUrl + 'apps';
     if (categoria) {
       url += `?categoria=${categoria}`;
     }
     return this.http.get<App[]>(url);
-    }
   }
+  //actualizar calificacion
+
+  actualizarCalificacion(appId: number, calificacion: any): Observable<HttpResponse<any>> {
+    let url = `${this.apiUrl}apps/${appId}/calificacion`;
+    url += `?calificacion=${calificacion}`;
+    const body = { calificacion: calificacion };
+    return this.http.put<any>(url, body, { observe: 'response' });
+  }
+}
